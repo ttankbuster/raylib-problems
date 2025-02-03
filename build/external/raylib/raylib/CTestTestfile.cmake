98203372de349a58eb1949a717e564c6c047ee39
Cyrus Harding <10121762+Cyrus-Harding@users.noreply.github.com>
2025-02-03 15:27:13 +0100

    Add .gitignore

--- build/external/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/teddy/Desktop/raylib dev/raylib-3/external/raylib/src
-# Build directory: C:/Users/teddy/Desktop/raylib dev/raylib-3/build/external/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
