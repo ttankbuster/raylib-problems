38f23c3ebc566a20f1fbb73490db77f2b4aa1cb8
ttankbuster <119669588+ttankbuster@users.noreply.github.com>
2025-02-03 10:50:39 +0000

    org1

---
+++ external/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/teddy/Desktop/raylib dev/raylib-3/external/raylib
+# Build directory: C:/Users/teddy/Desktop/raylib dev/raylib-3/build/external/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
